31c5417230f653f228f9780436b18d86bf239e48
pedro <151633598+pimentelcwb@users.noreply.github.com>
2025-04-12 13:32:52 -0300

    Update audios.py

--- audios.py
+++ audios.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 import os
 
 # Lê a API key de um arquivo
-with open("C:/Users/HP/OneDrive/PEDRO/ARTIGOS/BA_TESE/brand_activism_data_analysis/audio/openai_key.txt", "r") as f:
+with open("C:/include_your_path_here/openai_key.txt", "r") as f:
     api_key = f.read().strip()
 
 # Inicializa o cliente
@@ -10,12 +10,12 @@ client = OpenAI(api_key=api_key)
 
 # Abre o arquivo de áudio
 audio_files = [
-    "C:/Users/HP/OneDrive/PEDRO/ARTIGOS/BA_TESE/brand_activism_data_analysis/audio/WhatsApp Audio 2025-04-08 at 06.05.06.ogg",
-    "C:/Users/HP/OneDrive/PEDRO/ARTIGOS/BA_TESE/brand_activism_data_analysis/audio/WhatsApp Audio 2025-04-08 at 06.05.06 (1).ogg"
+    "C:/include_your_path_here/file.ogg",
+    "C:/include_your_path_here/file2.ogg"
 ]
 
 # Corrigido: nome da variável e criação de pasta
-output_folder = "C:/Users/HP/OneDrive/PEDRO/ARTIGOS/BA_TESE/brand_activism_data_analysis/audio/transcriptions"
+output_folder = "C:/include_your_path_here/file"
 os.makedirs(output_folder, exist_ok=True)
 
 # Agora processa cada áudio
